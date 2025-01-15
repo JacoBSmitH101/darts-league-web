da43826486f75fcc5673061a7255bec600930da7
JacoBSmitH101 <jacobsmith2005@hotmail.com>
2025-01-15 16:09:52 +0000

    Enhance MatchesPage and HomePage: Improve match status color logic, update layout for better readability, and add iframe for external content display.

--- src/app/home/page.js
+++ src/app/home/page.js
@@ -7,8 +7,17 @@ export default function HomePage() {
             <p>
                 This page will eventually show your stats, recent matches and
                 more... For now you can see your matches and the standings for
-                all divisions via the bar on the left.
+                all divisions via the bar at the top
             </p>
+
+            <iframe
+                src="https://challonge.com/d9k30b1r/module"
+                width="100%"
+                height="500"
+                frameborder="0"
+                scrolling="auto"
+                allowtransparency="true"
+            ></iframe>
         </div>
     );
 }
